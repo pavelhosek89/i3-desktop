73d576a9edc7010b05c8a6a8278c7165638900cd
Pavel Hošek <pavelhosek89@gmail.com>
2021-03-11 02:50:38 +0100

    Backup - update backup script - delete install package libusb-1.0-0-dev

--- backup-profiles-data.sh
+++ backup-profiles-data.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
-echo -e "\e[1;35;40mBACKUP {Music,Videos,profiles,opt,etc}\e[0m"
+echo -e "\e[1;35;40mBACKUP {etc,opt,profiles,Music,Videos}\e[0m"
+
+BACKUP_FOLDER="/mnt/encrypted/$(hostname)"
+FILE_ETC="backup/etc"
+FILE_OPT="backup/opt"
+FILE_PROFILES="backup/profiles"
+
+if [ -d "$BACKUP_FOLDER" ]; then
+	if [ -f "$FILE_ETC" ]; then
+		echo -e "\n\e[1;31mETC\e[0m"
+		sudo rsync -ahrv --delete --dry-run --files-from=$FILE_ETC \
+		    /etc/ $BACKUP_FOLDER/etc/
+	fi
+
+	if [ -f "$FILE_OPT" ]; then
+		echo -e "\n\e[1;31mOPT\e[0m"
+		sudo rsync -ahrv --delete --dry-run --files-from=$FILE_OPT \
+		    /opt/ $BACKUP_FOLDER/opt/
+	fi
+
+	if [ -f "$FILE_PROFILES" ]; then
+		echo -e "\n\e[1;31mPROFILES\e[0m"
+		sudo rsync -ahrv --delete --dry-run --files-from=$FILE_PROFILES \
+		    /home/ $BACKUP_FOLDER/profiles/
+	fi
+fi
 
 echo -e "\n\e[1;31mMUSIC\e[0m"
-rsync -ahrv --delete \
+rsync -ahrv \
     /home/$USER/Music/ /mnt/disk/Music/
 
 echo -e "\n\e[1;31mVIDEOS\e[0m"
 rsync -ahrv \
     /home/$USER/Videos/ /mnt/disk/Videos/
-
-echo -e "\n\e[1;31mPROFILES\e[0m"
-sudo rsync -ahrv --delete --files-from=backup/profiles \
-    /home/ /mnt/encrypted/profiles/
-
-echo -e "\n\e[1;31mOPT\e[0m"
-sudo rsync -ahrv --delete --files-from=backup/opt \
-    /opt/ /mnt/encrypted/opt/
-
-echo -e "\n\e[1;31mETC\e[0m"
-sudo rsync -ahrv --delete --files-from=backup/etc \
-    /etc/ /mnt/encrypted/etc/
