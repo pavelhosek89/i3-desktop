541b1c9fec65a07339a1c574fbf6c8a8721df1aa
Pavel Hošek <pavelhosek89@gmail.com>
2021-07-09 21:33:54 +0200

    hotfix

--- backup-profiles-data.sh
+++ backup-profiles-data.sh
@@ -10,19 +10,19 @@ FILE_PROFILES="backup/profiles"
 if [ -d "$BACKUP_FOLDER" ]; then
 	if [ -f "$FILE_ETC" ]; then
 		echo -e "\n\e[1;31mETC\e[0m"
-		sudo rsync -ahrv --delete --dry-run --files-from=$FILE_ETC \
+		sudo rsync -ahrv --delete --files-from=$FILE_ETC \
 		    /etc/ $BACKUP_FOLDER/etc/
 	fi
 
 	if [ -f "$FILE_OPT" ]; then
 		echo -e "\n\e[1;31mOPT\e[0m"
-		sudo rsync -ahrv --delete --dry-run --files-from=$FILE_OPT \
+		sudo rsync -ahrv --delete --files-from=$FILE_OPT \
 		    /opt/ $BACKUP_FOLDER/opt/
 	fi
 
 	if [ -f "$FILE_PROFILES" ]; then
 		echo -e "\n\e[1;31mPROFILES\e[0m"
-		sudo rsync -ahrv --delete --dry-run --files-from=$FILE_PROFILES \
+		sudo rsync -ahrv --delete --files-from=$FILE_PROFILES \
 		    /home/ $BACKUP_FOLDER/profiles/
 	fi
 fi
